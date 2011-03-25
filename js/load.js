15e1c9ab5582a3384978ad4931aef75f231324d1
joemaddalone <joe@insert-title.com>
2011-03-25 09:35:57 -0700

    Edited js/load.js via GitHub

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@ function inc(jspath) {
     document.write('<script type="text/javascript" src="' + jspath + '"><\/script>');
 }
 inc("js/inserttitle.js");
-inc("js/compiled.uncomp.js");
+inc("js/compiled.js");
 
 
 
